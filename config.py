5f0625d12a80399b8eaab5ab5d7cbe04dce33f98
Andres16Ramirez <andresfeliperamirezespinal@gmail.com>
2026-07-08 12:27:48 -0500

    Tiempo aleatorio entre envio de correos

--- config.py
+++ config.py
@@ -6,4 +6,18 @@ load_dotenv()
 EMAIL = os.getenv("EMAIL")
 APP_PASSWORD = os.getenv("APP_PASSWORD")
 CV_PATH = os.getenv("CV_PATH")
-DELAY_SECONDS = int(os.getenv("DELAY_SECONDS", "30"))
+
+DELAY_MIN_SECONDS = int(os.getenv("DELAY_MIN_SECONDS", "30"))
+DELAY_MAX_SECONDS = int(os.getenv("DELAY_MAX_SECONDS", "59"))
+
+if not EMAIL:
+    raise ValueError("Falta EMAIL en el archivo .env")
+
+if not APP_PASSWORD:
+    raise ValueError("Falta APP_PASSWORD en el archivo .env")
+
+if not CV_PATH:
+    raise ValueError("Falta CV_PATH en el archivo .env")
+
+if DELAY_MIN_SECONDS > DELAY_MAX_SECONDS:
+    raise ValueError("DELAY_MIN_SECONDS no puede ser mayor que DELAY_MAX_SECONDS")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
+import random
 
-from config import DELAY_SECONDS
+from config import DELAY_MIN_SECONDS, DELAY_MAX_SECONDS, CV_PATH
 from services.excel_service import ExcelService
 from services.gmail_service import GmailService
 from services.logger_service import LoggerService
@@ -11,10 +12,7 @@ gmail = GmailService()
 with open("templates/correo.html", encoding="utf-8") as file:
     body = file.read()
 
-CV_PATH = "attachments/CV_AndresRamirez.pdf"
-
 excel = ExcelService("data/empresas.xlsx")
-
 companies = excel.get_companies()
 
 logger.info(f"Se encontraron {len(companies)} empresas.")
@@ -23,18 +21,21 @@ for company in companies:
 
     logger.info(f"Enviando correo a {company['empresa']}")
 
-    gmail.send_email(
+    sent = gmail.send_email(
         recipient=company["correo"],
         subject="QA Automation Engineer | Andrés Felipe Ramírez Espinal",
         body=body,
         attachment_path=CV_PATH
-
     )
 
-    logger.success(f"Correo enviado a {company['empresa']}")
+    if sent:
+        logger.success(f"Correo enviado a {company['empresa']}")
+    else:
+        logger.error(f"No se pudo enviar el correo a {company['empresa']}")
 
-    logger.wait(DELAY_SECONDS)
+    delay = random.randint(DELAY_MIN_SECONDS, DELAY_MAX_SECONDS)
 
-    time.sleep(DELAY_SECONDS)
+    logger.wait(delay)
+    time.sleep(delay)
 
 logger.success("Proceso finalizado.")
