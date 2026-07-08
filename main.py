b3d3b9c36479d83be57f07fb0518db2020163ca0
Andres16Ramirez <andresfeliperamirezespinal@gmail.com>
2026-07-08 12:37:38 -0500

    Tiempo aleatorio entre envio de correos fix

--- main.py
+++ main.py
@@ -15,15 +15,26 @@ with open("templates/correo.html", encoding="utf-8") as file:
 excel = ExcelService("data/empresas.xlsx")
 companies = excel.get_companies()
 
+subjects = [
+    "QA Automation Engineer | Andrés Felipe Ramírez Espinal",
+    "QA Tester / Automation Engineer | Andrés Ramírez",
+    "QA Analyst | Testing Manual y Automatización | Andrés Ramírez",
+    "QA Engineer | Automatización, SQL y Jira | Andrés Ramírez",
+    "QA Tester | Experiencia en automatización | Andrés Ramírez"
+]
+
 logger.info(f"Se encontraron {len(companies)} empresas.")
 
 for company in companies:
 
     logger.info(f"Enviando correo a {company['empresa']}")
+    
+    subject = random.choice(subjects)
+
 
     sent = gmail.send_email(
         recipient=company["correo"],
-        subject="QA Automation Engineer | Andrés Felipe Ramírez Espinal",
+        subject=subject,
         body=body,
         attachment_path=CV_PATH
     )
